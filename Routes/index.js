4887b88ca67e7524189cf39a981ea205b4d12ddd
Rasagnya <gudipudirasagnya@gmail.com>
2022-07-10 13:23:27 +0530

    uploading the starter files

--- Routes/index.js
+++ Routes/index.js
@@ -1,13 +1,2 @@
 const express = require("express");
-const router = express.Router();
 const cors = require("cors");
-const app = express();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(router);
-
-router.use("/auth", require("./auth"));
-
-module.exports = app;
